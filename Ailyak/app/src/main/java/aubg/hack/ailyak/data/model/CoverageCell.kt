6602b6c70088d0e285ce998ab924f9c063542a79
Lyubomira Staninska <106514492+Bubomira@users.noreply.github.com>
2026-03-28 13:58:26 +0200

    added data models

---
+++ Ailyak/app/src/main/java/aubg/hack/ailyak/data/model/CoverageCell.kt
@@ -0,0 +1,14 @@
+package aubg.hack.ailyak.data.model
+
+data class CoverageCell (
+   val latmin: Double,
+    val lonmin:Double,
+    val latmax:Double,
+    val lonmax:Double,
+   //Mobile country code
+    val mcc: Int,
+   //Mobile network code, ex A1's code
+    val mnc:Int
+
+
+)
