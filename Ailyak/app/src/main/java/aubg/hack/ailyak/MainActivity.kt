cbed5d30337c79c8464160ddde330e19b019c5ac
StoyanChorbov <stoyan.chorbov@protonmail.com>
2026-03-28 13:56:40 +0200

    Added map

--- Ailyak/app/src/main/java/aubg/hack/ailyak/MainActivity.kt
+++ Ailyak/app/src/main/java/aubg/hack/ailyak/MainActivity.kt
@@ -7,18 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
+import aubg.hack.ailyak.ui.components.SurvivalMap
 import aubg.hack.ailyak.ui.theme.AilyakTheme
 import com.mapbox.android.core.permissions.PermissionsManager
 
 class MainActivity : ComponentActivity() {
-
-    lateinit var permissionsManager: PermissionsManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AilyakTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    SomeFunction(Modifier.padding(innerPadding))
+                    SurvivalMap(Modifier.fillMaxSize().padding(innerPadding))
                 }
             }
         }
---
+++ Ailyak/app/src/main/java/aubg/hack/ailyak/ui/components/Map.kt
@@ -0,0 +1,30 @@
+package aubg.hack.ailyak.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.mapbox.maps.extension.compose.MapEffect
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.locationcomponent.createDefault2DPuck
+import com.mapbox.maps.plugin.locationcomponent.location
+
+@Composable
+fun SurvivalMap(modifier: Modifier) {
+    val mapViewportState = rememberMapViewportState()
+
+    MapboxMap(
+        modifier = modifier,
+        mapViewportState = mapViewportState
+    ) {
+        MapEffect(Unit) { mapView ->
+            mapView.location.updateSettings {
+                locationPuck = createDefault2DPuck(withBearing = true)
+                enabled = true
+                puckBearing = PuckBearing.COURSE
+                puckBearingEnabled = true
+            }
+            mapViewportState.transitionToFollowPuckState()
+        }
+    }
+}
