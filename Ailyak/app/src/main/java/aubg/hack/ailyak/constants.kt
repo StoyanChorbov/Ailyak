102fa54e5f3acb0a8759d77f1a13a205c2b80814
Lyubomira Staninska <106514492+Bubomira@users.noreply.github.com>
2026-03-28 17:23:53 +0200

    cell tower service

--- Ailyak/app/src/main/java/aubg/hack/ailyak/constants.kt
+++ Ailyak/app/src/main/java/aubg/hack/ailyak/constants.kt
@@ -2,4 +2,8 @@ package aubg.hack.ailyak
 
 object CellConstants{
     const val apiUrl=  "https://opencellid.org"
+    //Default values, will be calculated later
+    const val latituteRangeDefault:Double = 1000.00
+    const val longitudeRangeDefault:Double=1000.00
+
 }
