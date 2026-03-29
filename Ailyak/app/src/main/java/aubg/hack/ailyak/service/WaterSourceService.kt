86627632f5bce76350a72c4c77e425dca4134f4f
Lyubomira Staninska <106514492+Bubomira@users.noreply.github.com>
2026-03-29 04:27:17 +0300

    fix

--- Ailyak/app/src/main/java/aubg/hack/ailyak/service/WaterSourceService.kt
+++ Ailyak/app/src/main/java/aubg/hack/ailyak/service/WaterSourceService.kt
@@ -4,7 +4,7 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
-interface WaterApiService {
+interface WaterSourceService {
     @FormUrlEncoded
     @POST("interpreter")
     suspend fun queryWater(@Field("data") query: String): OverpassResponse
